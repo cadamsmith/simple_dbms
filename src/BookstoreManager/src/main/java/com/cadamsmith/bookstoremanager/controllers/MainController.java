78e847b892b24485740deee77f4ff327bf7c07f7
cadamsmith <cadamsmith.dev@gmail.com>
2022-08-03 09:27:29 -0500

    remove unused import

--- src/BookstoreManager/src/main/java/com/cadamsmith/bookstoremanager/controllers/MainController.java
+++ src/BookstoreManager/src/main/java/com/cadamsmith/bookstoremanager/controllers/MainController.java
@@ -10,7 +10,6 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 import java.util.List;
